dd3040554aaddc704d49190e3460c07b1e3e03a6
user <hkiang01@yahoo.com>
2015-11-05 13:03:47 -0600

    simple module

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='483-mp3',
+      version='0.0.1',
+      description='Link state and distance vector routing protocols',
+      url='https://github.com/hkiang01/438-mp3',
+      py_modules=['routing'],
+      )
