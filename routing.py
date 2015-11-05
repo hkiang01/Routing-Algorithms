abaaddc3f7f0ece4b3a8ad0407a068cfa11b43c3
user <hkiang01@yahoo.com>
2015-11-05 14:49:55 -0600

    draft of graph construction via parsing topology file

--- routing.py
+++ routing.py
@@ -1,33 +1,54 @@
 import sys
 import getopt
 
+class Node:
+	def __init__(self, ID=-1):
+		self.links = [] # links node is connected to
+		self.ID = ID
+
+class Link:
+	def __init__(self, destID=-1, cost=0):
+		self.destID = destID
+		self.cost = cost
+
 class Graph:
 	def __init__(nodes):
 		self.nodes = []
 	def addNode(node):
 		self.nodes.append(node)
+		return Node
+	# finds nodes in graph matching nodeId.
+	# if no match, creates new node and appends to graph
+	# returns node(s) matched or created
+	def findNode(nodeID):
+		res = filter(lamda x: x.ID == nodeID, self.nodes)
+		if not res:
+			temp = Node(nodeID)
+			res = self.addNode(temp)
+		return res
+	# adds links between 2 nodes, which are found or created
+	# comprehensive
+	def addLink(sourceID, destID, cost):
+		sourceNode = self.findNode(sourceID)
+		destNode = self.findNode(destID)
 
-class Node:
-	def __init__(self, ID=-1):
-		self.links = [] # links node is connected to
-		self.ID = ID
-	def addLink(self, newlink):
-		self.links.append(newlink)
+		sourceLink = Link(destId, cost) #sourceID to destID
+		sourceNode.links.append(sourceLink)
 
-class Link:
-	def __init__(self, sourceID=-1, destID=-1, cost=0):
-		self.sourceID = sourceID
-		self.destID = destID
-		self.cost = cost
-
-links = [] # list of links
+		destLink = Link(sourceId, cost) #distID to sourceid
+		destNode.links.append(destLink)		
 
 # file parsing and topology construction
 def parse(filename, filetype, graph):
+	f = open(filename, "r")
 	if(filetype == 'TOPOLOGY_FILE'): # process the links
+		#format: <ID of a node> <ID of another node> <cost of link>
 		num_nodes = file_len(filenam)
 		for i in xrange(0, num_nodes):
-			links.
+			currLine = f.readline()
+			# e.g., currline 1 2 3 to ['1', '2', '3'] to currlink [1,2,3]
+			currLink = map(int, currLine.split())
+			graph.addLink(currLink[0], currLink[1], currLink[2])
 	elif(filetype == 'TOPOLOGY_CHANGES_FILE'):
 
 	elif(filetype == 'MESSAGE_FILE'):
