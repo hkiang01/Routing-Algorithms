cc85058d35ba1397220c77a0528d762e6da0dea2
jbigg <jbigg.2012@gmail.com>
2015-11-05 21:24:39 -0600

    Added handling of topology changes file

--- routing.py
+++ routing.py
@@ -11,6 +11,22 @@ class Node:
 		print 'Node: %d - (Neighbor:cost): ' % (self.ID)
 		for link in self.links:
 			print "%d:%d" % (link.destID, link.cost)
+	def neighbors(self):
+		result = []
+		for link in self.links:
+			result.append(link.destID)
+		return result
+	def setLink(self,destID,cost):
+		if not destID in self.neighbors():
+			return
+		link = None
+		for zelda in self.links:
+			if zelda.destID == destID:
+				link = zelda
+		if cost==-999:
+			self.links.remove(link)
+		else:
+			link.cost = cost
 	def addLinkToNode(self, link):
 		self.links.append(link)
 
@@ -28,25 +44,45 @@ class Graph:
 	# finds nodes in graph matching nodeId.
 	# if no match, creates new node and appends to graph
 	# returns node(s) matched or created
-	def findNode(self, nodeID):
+	def findNode(self, nodeID,autocreate=True):
 		res = [x for x in self.nodes if x.ID == nodeID]
 		for x in self.nodes:
 			if x.ID == nodeID:
 				return x
-		temp = Node(nodeID)
-		self.addNode(temp)
-		return self.findNode(nodeID)
+		if autocreate:
+			temp = Node(nodeID)
+			self.addNode(temp)
+			return self.findNode(nodeID)
+		else:
+			return None
 	# adds links between 2 nodes, which are found or created
 	# comprehensive
 	def addLink(self, sourceID, destID, cost):
 		sourceNode = self.findNode(sourceID)
 		destNode = self.findNode(destID)
 
+		if cost==-999: #if cost is -999, don't do anything
+			return
+
 		sourceLink = Link(destID, cost) #sourceID to destID
 		sourceNode.addLinkToNode(sourceLink)
 
 		destLink = Link(sourceID, cost) #distID to sourceid
 		destNode.addLinkToNode(destLink)
+	#changes a link between sourceID and destID to cost C. 
+	#if cost is -999, removes link
+	#if no such link exists, create a link
+	def changeLink(self, sourceID, destID, cost):
+		sourceNode = self.findNode(sourceID,False)
+		destNode = self.findNode(destID,False)
+		if sourceNode==None or destNode==None:
+			return
+		if not destID in sourceNode.neighbors():
+			self.addLink(sourceID,destID,cost)
+		else:
+			sourceNode.setLink(destID,cost)
+			destNode.setLink(sourceID,cost)
+		
 	def printGraph(self):
 		for node in self.nodes:
 			node.printNode()
@@ -64,6 +100,13 @@ def parse(filename, filetype, graph):
 			graph.addLink(currLink[0], currLink[1], currLink[2])
 	elif(filetype == 'TOPOLOGY_CHANGES_FILE'):
 		print 'parisng topology changes file...'
+		#format: <ID of a node> <ID of another node> <cost of link>
+		lines = f.readlines()
+		for line in lines:
+			currLink = map(int, line.split())
+			graph.changeLink(currLink[0], currLink[1], currLink[2])
+			print 'affecting change: %d <-> %d with cost %d' % (currLink[0],currLink[1],currLink[2])
+			graph.printGraph()
 	elif(filetype == 'MESSAGE_FILE'):
 		print 'parsing message file...'
 	else:
@@ -74,6 +117,7 @@ def main():
 	g = Graph()
 	parse('topology.txt', 'TOPOLOGY_FILE', g)
 	g.printGraph()
+	parse('changes.txt','TOPOLOGY_CHANGES_FILE',g)
 
 if __name__ == '__main__':
 	main()
